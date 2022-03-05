7a6278d7581190184e86597f19d8eaf9ed55552d
Random Guy <leekhoi5319990@gmail.com>
2022-03-05 12:24:24 +0700

    packages

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,12 +1,8 @@
 // augmenting default Session type
 
 import { UserSession } from './types';
-import { DefaultSession } from "next-auth";
 declare module "next-auth" {
     interface Session {
         user: UserSession;
-    }
-    interface User {
-        id: string;
     }
 }
